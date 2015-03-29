f17dc79f8b3ae8502b8d2aff8af16eed74cc3d9a
RituVyas <priyal.ritu@gmail.com>
2015-03-29 23:40:33 +0100

    First draft version
    Added first version

---
+++ FeedBackPopUp.js
@@ -0,0 +1,19 @@
+﻿function showFeedbackDialog(id) {
+    var div = document.getElementById(id);
+    if (div.style.display == 'block')
+        div.style.display = 'none';
+    else
+        div.style.display = 'block'
+}
+
+function hideFeedbackDialog(id) {
+    var div = document.getElementById(id);
+    div.style.display = 'none';
+
+}
+
+function sumbitFeedBack(id)
+{
+    var div = document.getElementById(id);
+    div.style.display = 'none';
+}
